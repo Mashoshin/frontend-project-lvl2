e8edbe3b04145f65a6efbef8e18eaa108a5062cc
Женя <zena@MacBook-Pro-Zena.local>
2019-11-04 19:19:31 +0300

    add a arguments and action

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+const program = require('commander');
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .action()
+  .option('-f, --format [type]', 'Output format');
 
 program.parse(process.argv);
